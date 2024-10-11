7eeb2119aa32b4191d31959ebefb647581753213
Andrey <andrey_baldin2107@mail.ru>
2024-10-11 21:40:19 +0700

    Лабораторная 2
    рабочая версия

---
+++ Лаб 2(2).cpp
@@ -0,0 +1,233 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+#include <string>
+#include <iostream>
+
+int fields1, animals1;
+
+class Plant {
+private:
+    std::string name;
+    int growthTime; // Время роста в днях
+    int yield; // Урожайность
+
+public:
+    Plant(std::string name = "None", int growthTime = 0, int yield = 0)
+        : name(name), growthTime(growthTime), yield(yield) {}
+
+    void inputPlant() {
+        std::cout << "Введите название растения: ";
+        std::cin >> name;
+        std::cout << "Введите время роста (в днях): ";
+        std::cin >> growthTime;
+        std::cout << "Введите урожайность: ";
+        std::cin >> yield;
+    }
+
+    void printPlant() const {
+        std::cout << "Растение: " << name << ", Время роста: " << growthTime << " дней, Урожайность: " << yield << std::endl;
+    }
+
+    int getYieldPlant() const {
+        return yield;
+    }
+};
+
+class Animal {
+private:
+    std::string name;
+    int age; // Возраст в годах
+    int productivity; // Продуктивность (например, количество молока в день)
+
+public:
+    Animal(std::string name = "None", int age = 0, int productivity = 0)
+        : name(name), age(age), productivity(productivity) {}
+
+    void inputAnimal() {
+        std::cout << "Введите название животного: ";
+        std::cin >> name;
+        std::cout << "Введите возраст животного: ";
+        std::cin >> age;
+        std::cout << "Введите количество кг продукта, которое производит животное: ";
+        std::cin >> productivity;
+    }
+
+    void printAnimal() const {
+        std::cout << "Животное: " << name << ", Возраст: " << age << " лет, Количество продукта в кг: " << productivity << std::endl;
+    }
+
+    int getProductivityAnimal() const {
+        return productivity;
+    }
+};
+
+class Field {
+private:
+    int id;
+    Plant plant;
+    bool isPlanted; // true если поле засажено, false если нет
+
+public:
+    Field(int id = 0, Plant plant = Plant(), bool isPlanted = false)
+        : id(id), plant(plant), isPlanted(isPlanted) {}
+
+    void inputField() {
+        std::cout << "Введите номер поля: ";
+        std::cin >> id;
+        std::cout << "Поле засажено? (1 - да, 0 - нет): ";
+        std::cin >> isPlanted;
+        if (isPlanted) {
+            plant.inputPlant();
+        }
+        else {
+            plant = Plant();
+        }
+    }
+
+    void printField() const {
+        std::cout << "Поле " << id << ": ";
+        if (isPlanted) {
+            plant.printPlant();
+        }
+        else {
+            std::cout << "Поле не засажено" << std::endl;
+        }
+    }
+
+    int harvestField() {
+        if (isPlanted) {
+            isPlanted = false;
+            return plant.getYieldPlant();
+        }
+        return 0;
+    }
+};
+
+class Storage {
+private:
+    int plantYield;
+    int animalProducts;
+
+public:
+    Storage(int plantYield = 0, int animalProducts = 0)
+        : plantYield(plantYield), animalProducts(animalProducts) {}
+
+    void printStorage() const {
+        std::cout << "\nСклад: Урожай растений: " << plantYield << ", Продукты животных: " << animalProducts << std::endl;
+    }
+
+    void addPlantYieldStorage(int yield) {
+        plantYield += yield;
+    }
+
+    void addAnimalProductsStorage(int products) {
+        animalProducts += products;
+    }
+};
+
+class Farm {
+private:
+    Field fields[5];
+    Animal animals[5];
+    Storage storage;
+public:
+    Farm() {
+        for (int i = 0; i < fields1; i++) {
+            fields[i] = Field(i);
+        }
+        for (int i = 0; i < animals1; i++) {
+            animals[i] = Animal();
+        }
+    }
+
+    void input() {
+        for (int i = 0; i < fields1; ++i) {
+            std::cout << "\nВведите данные для поля\n";
+            fields[i].inputField();
+        }
+        for (int i = 0; i < animals1; ++i) {
+            std::cout << "\nВведите данные для животного\n";
+            animals[i].inputAnimal();
+        }
+    }
+
+    void print() const {
+        std::cout << "\nИнформация о ферме\n";
+        std::cout << "Поля:\n";
+        for (int i = 0; i < fields1; ++i) {
+            fields[i].printField();
+        }
+        std::cout << "\nЖивотные:\n";
+        for (int i = 0; i < animals1; ++i) {
+            animals[i].printAnimal();
+        }
+        storage.printStorage();
+    }
+
+    void logic() {
+        for (int i = 0; i < 2; ++i) {
+            storage.addPlantYieldStorage(fields[i].harvestField());
+        }
+        for (int i = 0; i < 2; ++i) {
+            storage.addAnimalProductsStorage(animals[i].getProductivityAnimal());
+        }
+        printf("\nПередача на склад\n");
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    system("color F0");
+    
+    printf("Ферма\n");
+    printf("Введите количество полей:\n");
+    scanf("%d", &fields1);
+    printf("Введите количество животных:\n");
+    scanf("%d", &animals1);
+    Farm myFarm;
+    // Ввод данных о ферме
+    myFarm.input();
+
+    // Вывод информации о ферме
+    myFarm.print();
+
+    // Выполняем основную логику 
+    myFarm.logic();
+
+    // Выводим обновленную информацию о ферме
+    myFarm.print();
+
+    // Работа с динамическим массивом объектов класса
+    fields1 = 1; animals1 = 1;
+    Farm* dynamicFarms = new Farm[2];
+    printf("\nРабота с динамическим массивом объектов класса\n");
+    for (int i = 0; i < 2; ++i) {
+        std::cout << "\nВведите данные для фермы " << i + 1<< ":\n";
+        dynamicFarms[i].input();
+    }
+    for (int i = 0; i < 2; ++i) {
+        dynamicFarms[i].print();
+        dynamicFarms[i].logic();
+        dynamicFarms[i].print();
+    }
+    delete[] dynamicFarms;
+
+    // Работа с массивом динамических объектов класса
+    printf("\nРабота с массивом динамических объектов класса\n");
+    Farm* dynamicFarmArray[2];
+    for (int i = 0; i < 2; ++i) {
+        dynamicFarmArray[i] = new Farm();
+        std::cout << "\nВведите данные для фермы " << i << ":\n";
+        dynamicFarmArray[i]->input();
+    }
+    for (int i = 0; i < 2; ++i) {
+        dynamicFarmArray[i]->print();
+        dynamicFarmArray[i]->logic();
+        dynamicFarmArray[i]->print();
+        delete dynamicFarmArray[i];
+    }
+
+    return 0;
+}
---
+++ Лаб 2.cpp
@@ -0,0 +1,205 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+// Структура для представления растения
+typedef struct {
+    char name[20];
+    int growthTime; // Время роста в днях
+    int yield; // Урожайность
+} Plant;
+
+// Структура для представления животного
+typedef struct {
+    char name[20];
+    int age; // Возраст в годах
+    int productivity; // Продуктивность (например, количество молока в л в день)
+} Animal;
+
+// Структура для представления поля
+typedef struct {
+    int id;
+    Plant plant;
+    int isPlanted; // 1 если поле засажено, 0 если нет
+} Field;
+
+// Структура для представления склада
+typedef struct {
+    int plantYield;
+    int animalProducts;
+} Storage;
+
+// Структура для представления фермы
+typedef struct {
+    Field fields[10];
+    Animal animals[10];
+    Storage storage;
+} Farm;
+
+// Инициализации 
+Plant createPlant(const char* name, int growthTime, int yield) {
+    Plant plant;
+    strcpy(plant.name, name);
+    plant.growthTime = growthTime;
+    plant.yield = yield;
+    return plant;
+}
+
+Animal createAnimal(const char* name, int age, int productivity) {
+    Animal animal;
+    strcpy(animal.name, name);
+    animal.age = age;
+    animal.productivity = productivity;
+    return animal;
+}
+
+Field createField(int id, Plant plant, int isPlanted) {
+    Field field;
+    field.id = id;
+    field.plant = plant;
+    field.isPlanted = isPlanted;
+    return field;
+}
+
+Storage createStorage(int plantYield, int animalProducts) {
+    Storage storage;
+    storage.plantYield = plantYield;
+    storage.animalProducts = animalProducts;
+    return storage;
+}
+
+Farm createFarm() {
+    Farm farm;
+    for (int i = 0; i < 2; i++) {
+        farm.fields[i] = createField(i, createPlant("", 0, 0), 0);
+        farm.animals[i] = createAnimal("", 0, 0);
+    }
+    farm.storage = createStorage(0, 0);
+    return farm;
+}
+
+// Функции для ввода
+void inputPlant(Plant* plant) {
+    while (getchar() != '\n');
+    printf("Введите название растения: ");
+    fgets(plant->name,20,stdin);
+    printf("Введите время роста (в днях): ");
+    scanf("%d", &plant->growthTime);
+    printf("Введите урожайность: ");
+    scanf("%d", &plant->yield);
+}
+
+void inputAnimal(Animal* animal) {
+    while (getchar() != '\n');
+    printf("Введите название животного: ");
+    fgets(animal->name, 20, stdin);
+    printf("Введите возраст животного: ");
+    scanf("%d", &animal->age);
+    printf("Введите количество кг продукта, которое произведит животное: ");
+    scanf("%d", &animal->productivity);
+}
+
+void inputField(Field* field) {
+    printf("Введите номер поля: ");
+    scanf("%d", &field->id);
+    printf("Поле засажено? (1 - да, 0 - нет): ");
+    scanf("%d", &field->isPlanted);
+    if (field->isPlanted) {
+        inputPlant(&field->plant);
+    }
+    else {
+        field->plant = createPlant("", 0, 0);
+    }
+}
+
+void inputFarm(Farm* farm, int flieds, int animals) {
+    for (int i = 0; i < flieds; i++) {
+        printf("\nВведите данные для поля\n");
+        inputField(&farm->fields[i]);
+    }
+    for (int j = 0; j < animals; j++) {
+        printf("\nВведите данные для животного\n");
+        inputAnimal(&farm->animals[j]);
+    }
+}
+
+// Функции для вывода
+
+void printPlant(Plant plant) {
+    printf("Растение: %sВремя роста: %d дней, Количество урожая: %d\n", plant.name, plant.growthTime, plant.yield);
+}
+
+void printAnimal(Animal animal) {
+    printf("\nЖивотное: %sВозраст: %d лет, Продукты(в кг): %d\n", animal.name, animal.age, animal.productivity);
+}
+
+void printField(Field field) {
+    printf("\nПоле %d: ", field.id);
+    if (field.isPlanted) {
+        printPlant(field.plant);
+    }
+    else {
+        printf("Поле не засажено\n");
+    }
+}
+
+void printStorage(Storage storage) {
+    printf("\nСклад: Урожай растений: %d, Продукты животных: %d кг\n", storage.plantYield, storage.animalProducts);
+}
+
+void printFarm(Farm farm, int flieds, int animals) {
+    printf("\nИнформация о ферме\n");
+    printf("Поля:\n");
+    for (int i = 0; i < flieds; i++) {
+        printField(farm.fields[i]);
+    }
+    printf("\nЖивотные\n");
+    for (int i = 0; i < animals; i++) {
+        printAnimal(farm.animals[i]);
+    }
+    printStorage(farm.storage);
+}
+
+// Основная логика 
+void logic(Farm* farm, int flieds, int animals) {
+    // Пример бизнес логики: сбор урожая и продукции животных
+    for (int i = 0; i < flieds; i++) {
+        if (farm->fields[i].isPlanted) {
+            farm->storage.plantYield += farm->fields[i].plant.yield;
+            farm->fields[i].isPlanted = 0; // Поле становится свободным после сбора урожая
+        }
+    }
+    for (int j = 0; j < animals; j++) {
+        farm->storage.animalProducts += farm->animals[j].productivity;
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    system("color F0");
+    int fields, animals;
+    Farm myFarm = createFarm();
+    
+    //Ввод количества полей и животных
+    printf("Ферма\n");
+    printf("Введите количество полей:\n");
+    scanf("%d", &fields);
+    printf("Введите количество животных:\n");
+    scanf("%d", &animals);
+
+    // Ввод данных о ферме
+    inputFarm(&myFarm, fields, animals);
+
+    // Вывод информации о ферме
+    printFarm(myFarm, fields, animals);
+
+    // Выполняем основную логику игры
+    logic(&myFarm, fields, animals);
+
+    // Выводим обновленную информацию о ферме
+    printFarm(myFarm, fields, animals);
+
+    return 0;
+}
